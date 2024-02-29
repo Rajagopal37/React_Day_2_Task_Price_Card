36f22a54c9c9931cc211bbeb9318402aeb2ecd4e
Rajagopal37 <trajagopal37@gmail.com>
2024-02-29 15:10:50 +0530

    React_Task_day_2_Price_Card

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import ReactCard from "./Components/Card"
+
+
+function App() {
+  
+  const datas = [
+    {
+      cardName: "Free",
+      price:"$0/Month"
+    },
+    {
+      cardName: "plus",
+      price:"$9/Month"
+    },
+    {
+      cardName: "Pro",
+      price:"$49/Month"
+    },
+  ]
+
+  return ( 
+    <div className="container mt-5 px-4 py-2 bg-primary  bg-gradient">
+        <div className="row">
+            <div className="col p-3 d-flex justify-content-center bg-secondary">
+                {  datas.map((data, index)=>(
+                <ReactCard key={index} cardName={data.cardName} price={data.price}
+                />)) }
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default App
---
+++ src/Components/Card.jsx
@@ -0,0 +1,35 @@
+import "../App.css"
+
+export default function ReactCard(props) {
+
+    const conbg = { }
+   
+
+  return (
+    <div className="card">
+        <div className="card-header bg-white">
+            <div className="cart-text d-flex justify-content-center text-secondary"> {props.cardName} </div>
+            <div className="card-title d-flex justify-content-center"><h3>{props.price}</h3></div>
+        </div>
+        <div className="card-body d-flex justify-content-center">
+            <ul className="list-item" style={conbg}>
+                <li>Single User</li>
+                <li>50GB Storage</li>
+                <li>Unlimited Public Projects</li>
+                <li>Community Access</li>
+                <li>Unlimited Private Projects</li>
+                <li>Dedicated Phone Support</li>
+                <li>Free Subdomain</li>
+                <li>Monthly Status Reports</li>
+            </ul>
+        </div>                    
+        <div className="card-footed">
+            <div className="col mb-3 d-flex justify-content-center">
+                <button className="btn btn-primary"  type="submit">Button</button>
+            </div>
+        </div>  
+    </div>
+  )
+}
+
+                    
